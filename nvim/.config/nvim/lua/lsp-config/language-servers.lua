4767401298237fa5c27906665394ade8d76465eb
Zach Yeap <zachary.yeap1@gmail.com>
2023-01-26 15:37:39 -0800

    config: add mapping for line diagnostics

--- nvim/.config/nvim/lua/lsp-config/language-servers.lua
+++ nvim/.config/nvim/lua/lsp-config/language-servers.lua
@@ -4,6 +4,7 @@ local opts = { noremap=true, silent=true }
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
 vim.keymap.set('n', '<space>d', vim.diagnostic.setloclist, opts)
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer

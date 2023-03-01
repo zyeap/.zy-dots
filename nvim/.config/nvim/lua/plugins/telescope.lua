0c16e520b35676dd10fe18770c966d677a4b5cad
Zach Yeap <zachary.yeap1@gmail.com>
2023-03-01 11:14:04 -0800

    config: forgot to add config for telescope file browser

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -1,28 +1,34 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  cmd = "Telescope",
-  branch = "0.1.x",
-  keys = {
-    {'<leader>ff', '<CMD>Telescope find_files <CR>', desc = "find files"},
-    {'<leader>fs', '<CMD>Telescope live_grep <CR>', desc = "find string"},
-    {'<leader>fb', '<CMD>Telescope file_browser <CR>', desc = "file browser"},
-    {'<leader>gc', '<CMD>Telescope git_commits <CR>', desc = "find commits"},
-    {'<leader>gb', '<CMD>Telescope git_branches <CR>', desc = "find branches"},
-    {'<leader>gs', '<CMD>Telescope git_status <CR>', desc = "find git status"},
-  },
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-telescope/telescope-file-browser.nvim"
-  },
-  config = function()
-    require("telescope").setup({
-    defaults = {
-      file_ignore_patterns = {
-        "node_modules",
-        "venv",
-        "__pycache__"
-      }
-    },
-  })
-  end
+	"nvim-telescope/telescope.nvim",
+	cmd = "Telescope",
+	branch = "0.1.x",
+	keys = {
+		{ "<leader>ff", "<CMD>Telescope find_files <CR>", desc = "find files" },
+		{ "<leader>fs", "<CMD>Telescope live_grep <CR>", desc = "find string" },
+		{ "<leader>fb", "<CMD>Telescope file_browser <CR>", desc = "file browser" },
+		{ "<leader>gc", "<CMD>Telescope git_commits <CR>", desc = "find commits" },
+		{ "<leader>gb", "<CMD>Telescope git_branches <CR>", desc = "find branches" },
+		{ "<leader>gs", "<CMD>Telescope git_status <CR>", desc = "find git status" },
+	},
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope-file-browser.nvim",
+	},
+	config = function()
+		require("telescope").setup({
+			defaults = {
+				file_ignore_patterns = {
+					"node_modules",
+					"venv",
+					"__pycache__",
+				},
+			},
+			extensions = {
+				file_browser = {
+					hijack_netrw = true,
+				},
+			},
+		})
+		require("telescope").load_extension("file_browser")
+	end,
 }

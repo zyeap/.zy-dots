27ea16507131e69a90009fc46036b7a5b462efec
Zach Yeap <zachary.yeap1@gmail.com>
2023-02-28 17:26:29 -0800

    config: adding toggleable terminal

---
+++ nvim/.config/nvim/lua/plugins/fterm.lua
@@ -0,0 +1,9 @@
+return {
+  "numToStr/FTerm.nvim",
+  keys = {
+    {'<leader>t', '<cmd>lua require("FTerm").toggle()<cr>', mode = {'n', 't'}, desc = 'Toggle terminal'},
+  },
+  opts = {
+    border = "rounded",
+  }
+}
